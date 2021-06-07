bc8e2984b6de462bb5ab25cc3fd79ad19b322600
htraing <traingheihong@gmail.com>
2021-06-07 21:19:15 +0200

    fist component in storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,13 @@
+import { defineCustomElements } from '../dist/esm/loader';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+defineCustomElements();
---
+++ src/stories/my-component.stories.js
@@ -0,0 +1,7 @@
+export default {
+  title: 'Demo',
+};
+
+export const MyComponent = () => {
+  return `<my-component first="Stencil" last="'Don't call me a framework' JS"></my-component>`;
+};
